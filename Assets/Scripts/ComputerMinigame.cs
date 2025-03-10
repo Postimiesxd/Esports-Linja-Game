af045e9a8ca84bcdd786fe9826dbac3baa3761c1
postimies <pyllykakkaowo@gmail.com>
2025-03-10 14:33:41 +0200

    lisää koodi ja jotian partikkeleita

--- Assets/Scripts/ComputerMinigame.cs
+++ Assets/Scripts/ComputerMinigame.cs
@@ -7,18 +7,47 @@ public class ComputerMinigame : MonoBehaviour
 {
     private int targetsHit = 0;
     private int totalTargets = 3;
+    public float celebrationDelay = 2f;
+    public GameObject confettiPrefab; // Assign this in Unity
 
     public void OnTargetHit()
     {
         targetsHit++;
         if (targetsHit >= totalTargets)
         {
-            EndMinigame();
+            StartCoroutine(CelebrateAndEnd());
         }
     }
 
-    void EndMinigame()
+    IEnumerator CelebrateAndEnd()
     {
-        SceneManager.LoadScene("MainGameScene"); // Change to your main game scene name
+        Debug.Log("Celebration started!");
+
+        if (confettiPrefab != null)
+        {
+            GameObject confetti = Instantiate(confettiPrefab, Camera.main.transform.position + new Vector3(0, 3, 5), Quaternion.identity);
+            confetti.SetActive(true);
+
+            ParticleSystem confettiParticles = confetti.GetComponent<ParticleSystem>();
+            if (confettiParticles != null)
+            {
+                confettiParticles.Play();
+                Debug.Log("Confetti playing!");
+            }
+            else
+            {
+                Debug.LogError("No ParticleSystem found on confettiPrefab!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Confetti prefab is null!");
+        }
+
+        Vector3 confettiPosition = Camera.main.transform.position + Camera.main.transform.forward * 5 + new Vector3(0, 0, 100);
+        Instantiate(confettiPrefab, confettiPosition, Quaternion.identity);
+
+        yield return new WaitForSeconds(celebrationDelay);
+        SceneManager.LoadScene("MainGameScene");
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,23 +6,15 @@ using UnityEngine.UI;
 public class Target : MonoBehaviour
 {
     private ComputerMinigame minigameManager;
-    private Image targetImage;
-
-    public Sprite hitSprite; // Assign in the Inspector
 
     void Start()
     {
         minigameManager = FindObjectOfType<ComputerMinigame>();
-        targetImage = GetComponent<Image>();
     }
 
     public void HitTarget()
     {
-        if (hitSprite != null)
-        {
-            targetImage.sprite = hitSprite; // Change the sprite
-        }
-        GetComponent<Button>().interactable = false; // Disable further clicks
+        gameObject.SetActive(false); // Hide target when clicked
         minigameManager.OnTargetHit();
     }
 }
